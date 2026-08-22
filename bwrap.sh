f65ff388f589c14588d2bc25b8b2c5735b681cf0
Eiji Sakai <eijisakai@gmail.com>
2026-08-22 12:28:40 +0700

    bwrap.shを追加
    bwrapのオプション構成を単体で試すためのシェルスクリプト。
    sandbox/を/sandboxとしてマウントし、bash -c "$1"でコマンドを実行する。

---
+++ bwrap.sh
@@ -0,0 +1,16 @@
+bwrap \
+--unshare-user-try \
+--unshare-net \
+--new-session \
+--ro-bind /usr /usr \
+--ro-bind /bin /bin \
+--ro-bind /lib /lib \
+--ro-bind /lib64 /lib64 \
+--ro-bind /etc /etc \
+--tmpfs /tmp \
+--proc /proc \
+--dev /dev \
+--setenv PATH /bin:/usr/bin:/bin \
+--bind /home/esakai/dev/claude/20260822/sandbox /sandbox \
+--chdir /sandbox \
+bash -c "$1"
